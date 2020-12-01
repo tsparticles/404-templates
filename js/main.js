a7591090f5d4eebd180dcb75d023a937dbfa24ed
Matteo Bruni <ar3s@icloud.com>
2020-12-01 18:30:59 +0100

    index pages are now loaded from a json

---
+++ js/main.js
@@ -0,0 +1,79 @@
+$(document).ready(function () {
+    var pages = $("#pages");
+
+    $.get('data/pages.json', function (data) {
+        $.each(data, function (idx, item) {
+            var page = $("<div>");
+
+            page.addClass("col-md-4");
+
+            var card = $("<div>");
+
+            card.addClass("card mb-4");
+            card.addClass("shadow-sm");
+
+            var image = $("<img>");
+
+            image.addClass("bd-placeholder-img");
+            image.addClass("card-img-top");
+
+            image.attr({
+                width: "100%",
+                height: 225,
+                focusable: false,
+                role: "img",
+                ariaLabel: item.description,
+                src: "__screenshots/" + item.image
+            });
+
+            card.append(image);
+
+            var cardBody = $("<div>");
+
+            cardBody.addClass("card-body");
+
+            var cardText = $("<div>");
+
+            cardText.addClass("card-text");
+
+            cardText.text(item.description);
+
+            cardBody.append(cardText);
+
+            var cardContent = $("<div>");
+
+            cardContent.addClass("d-flex");
+            cardContent.addClass("justify-content-between");
+            cardContent.addClass("align-items-center");
+
+            var btnGroup = $("<div>");
+
+            btnGroup.addClass("btn-group");
+
+            var btn = $("<a>");
+
+            btn.addClass("btn");
+            btn.addClass("btn-sm");
+            btn.addClass("btn-outline-secondary");
+
+            btn.text("View");
+
+            btn.attr({
+                href: item.url,
+                target: "_blank"
+            });
+
+            btnGroup.append(btn);
+
+            cardContent.append(btnGroup);
+
+            cardBody.append(cardContent);
+
+            card.append(cardBody);
+
+            page.append(card);
+
+            pages.append(page);
+        });
+    });
+});
