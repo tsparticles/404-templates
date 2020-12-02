24f666e21575eb86046a1112ac2a722027a9e1a2
Matteo Bruni <ar3s@icloud.com>
2020-12-02 10:28:28 +0100

    changed out mode and speed in masked 404

--- masked/js/404.js
+++ masked/js/404.js
@@ -1,72 +1,74 @@
 tsParticles.load("tsparticles", {
     background: {
-        color: "#fff"
+      color: "#fff"
     },
     fpsLimit: 60,
     interactivity: {
-        detectsOn: "window",
-        events: {
-            resize: true,
-            onHover: {
-                enable: true,
-                mode: "bubble"
-            }
-        },
-        modes: {
-            bubble: {
-                distance: 300,
-                opacity: 0.8,
-                size: 100
-            }
+      detectsOn: "window",
+      events: {
+        resize: true,
+        onHover: {
+          enable: true,
+          mode: "bubble"
         }
+      },
+      modes: {
+        bubble: {
+          distance: 300,
+          opacity: 0.8,
+          size: 100
+        }
+      }
     },
     particles: {
-        color: {
-            value: ["#f00", "#0f0", "#00f", "#ff0", "#0ff", "#f0f"],
-            animation: {
-                enable: true,
-                speed: 20,
-                sync: false
-            }
-        },
-        move: {
-            direction: "none",
-            enable: true,
-            outModes: {
-                default: "out"
-            },
-            random: false,
-            speed: 10,
-            straight: false
-        },
-        number: {
-            density: {
-                enable: true,
-                area: 800
-            },
-            value: 160
-        },
-        opacity: {
-            random: {
-                enable: true,
-                minimumValue: 0.2
-            },
-            value: 0.8
-        },
-        shape: {
-            type: "circle"
-        },
-        stroke: {
-            color: "#ff0000",
-            width: 0
-        },
-        size: {
-            random: {
-                enable: true,
-                minimumValue: 25
-            },
-            value: 50
+      color: {
+        value: ["#f00", "#0f0", "#00f", "#ff0", "#0ff", "#f0f"],
+        animation: {
+          enable: true,
+          speed: 40,
+          sync: false
         }
+      },
+      move: {
+        direction: "none",
+        enable: true,
+        outModes: {
+          default: "bounce"
+        },
+        random: false,
+        speed: 10,
+        straight: false
+      },
+      number: {
+        density: {
+          enable: true,
+          area: 800
+        },
+        value: 160
+      },
+      opacity: {
+        random: {
+          enable: true,
+          minimumValue: 0.2
+        },
+        value: 0.8
+      },
+      shape: {
+        type: "circle"
+      },
+      stroke: {
+        color: "#ff0000",
+        width: 0
+      },
+      size: {
+        random: {
+          enable: true,
+          minimumValue: 25
+        },
+        value: 50
+      }
     },
     detectRetina: true
-});
+  });
+  
+  
