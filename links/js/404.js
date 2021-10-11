349d4a1483f782d26a0c5f88edd7a3351b2c60cd
Matteo Bruni <176620+matteobruni@users.noreply.github.com>
2021-10-11 12:56:46 +0200

    links template

--- links/js/404.js
+++ links/js/404.js
@@ -5,35 +5,6 @@ var particles = {
         }
     },
     fpsLimit: 60,
-    interactivity: {
-        detectsOn: "canvas",
-        events: {
-            onClick: {
-                enable: true,
-                mode: "push"
-            },
-            onHover: {
-                enable: true,
-                mode: "repulse"
-            },
-            resize: true
-        },
-        modes: {
-            bubble: {
-                distance: 400,
-                duration: 2,
-                opacity: 0.8,
-                size: 40
-            },
-            push: {
-                quantity: 4
-            },
-            repulse: {
-                distance: 200,
-                duration: 0.4
-            }
-        }
-    },
     particles: {
         color: {
             value: "#ffffff"
