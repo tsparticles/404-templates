598291e505b897de94abd8a28027185c059fe9df
sunny <sunnyvales789@gmail.com>
2021-10-11 13:54:41 +0530

    Update 404.js

--- page-template/js/404.js
+++ page-template/js/404.js
@@ -1,57 +1,81 @@
 var particles = {
+    background: {
+        color: {
+            value: "#0d47a1"
+        }
+    },
     fpsLimit: 60,
-    particles: {
-        number: {
-            value: 160,
-            density: {
+    interactivity: {
+        detectsOn: "canvas",
+        events: {
+            onClick: {
                 enable: true,
-                area: 800
-            }
+                mode: "push"
+            },
+            onHover: {
+                enable: true,
+                mode: "repulse"
+            },
+            resize: true
         },
+        modes: {
+            bubble: {
+                distance: 400,
+                duration: 2,
+                opacity: 0.8,
+                size: 40
+            },
+            push: {
+                quantity: 4
+            },
+            repulse: {
+                distance: 200,
+                duration: 0.4
+            }
+        }
+    },
+    particles: {
         color: {
             value: "#ffffff"
+        },
+        links: {
+            color: "#ffffff",
+            distance: 150,
+            enable: true,
+            opacity: 0.5,
+            width: 1
+        },
+        collisions: {
+            enable: true
+        },
+        move: {
+            direction: "none",
+            enable: true,
+            outMode: "bounce",
+            random: false,
+            speed: 6,
+            straight: false
+        },
+        number: {
+            density: {
+                enable: true,
+                value_area: 800
+            },
+            value: 80
+        },
+        opacity: {
+            value: 0.5
         },
         shape: {
             type: "circle"
         },
-        opacity: {
-            value: 1,
-            random: {
-                enable: true,
-                minimumValue: 0.1
-            },
-            animation: {
-                enable: true,
-                speed: 1,
-                minimumValue: 0,
-                sync: false
-            }
-        },
         size: {
-            value: 3,
-            random: {
-                enable: true,
-                minimumValue: 1
-            }
-        },
-        move: {
-            enable: true,
-            speed: 0.17,
-            direction: "none",
             random: true,
-            straight: false,
-            outModes: {
-                default: "out"
-            },
-        }
-    },
-    interactivity: {
-        detectsOn: "canvas",
-        events: {
-            resize: false
+            value: 5
         }
     },
     detectRetina: true
 };
+
 
 tsParticles.load("tsparticles", particles);
