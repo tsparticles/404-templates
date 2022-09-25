17f820c86fba3fc82725efff0e8f39e411074b14
prathambagadiya <prathambagadiya6606@gmail.com>
2022-09-25 23:34:21 +0530

    page added
    Signed-off-by: prathambagadiya <prathambagadiya6606@gmail.com>

---
+++ 404 found/js/script.js
@@ -0,0 +1,56 @@
+tsParticles.load("tsparticles", {
+    fpsLimit: 60,
+    particles: {
+        number: {
+            value: 5,
+            density: {
+                enable: true,
+                area: 800
+            }
+        },
+        color: {
+            value: "#fcfcfc"
+        },
+        shape: {
+            type: "circle"
+        },
+        opacity: {
+            value: 0.5,
+            random: {
+                enable: true,
+                minimumValue: 0.1
+            },
+        },
+        size: {
+            value: 140,
+            random: {
+                enable: true,
+                minimumValue: 40
+            },
+            animation: {
+                enable: true,
+                speed: 10,
+                minimumValue: 40,
+                sync: false
+            }
+        },
+        move: {
+            enable: true,
+            speed: 8,
+            direction: "none",
+            random: false,
+            straight: false,
+            outModes: {
+                default: "out"
+            }
+        }
+    },
+    interactivity: {
+        detectsOn: "canvas",
+        events: {
+            resize: true
+        }
+    },
+    detectRetina: true
+});
+
