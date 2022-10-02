1abd319988f91e314f9b74c5da611993e6103633
Natalie de Weerd <nataliedeweerd@gmail.com>
2022-10-02 23:36:39 +0100

    Tidy up JS file
    Combined addClass functions to make the code neater.

--- js/main.js
+++ js/main.js
@@ -4,19 +4,13 @@ $(document).ready(function () {
     $.get('data/pages.json', function (data) {
         $.each(data, function (idx, item) {
             var page = $("<div>");
-
             page.addClass("col-md-4");
 
             var card = $("<div>");
-
-            card.addClass("card mb-4");
-            card.addClass("shadow-sm");
+            card.addClass("card mb-4 shadow-sm");
 
             var image = $("<img>");
-
-            image.addClass("bd-placeholder-img");
-            image.addClass("card-img-top");
-
+            image.addClass("bd-placeholder-img card-img-top");
             image.attr({
                 width: "100%",
                 height: 196,
@@ -29,50 +23,34 @@ $(document).ready(function () {
             card.append(image);
 
             var cardBody = $("<div>");
-
             cardBody.addClass("card-body");
 
             var cardText = $("<div>");
-
             cardText.addClass("card-text");
-
             cardText.text(item.description);
-
             cardBody.append(cardText);
 
             var cardContent = $("<div>");
-
             cardContent.addClass("d-flex");
             cardContent.addClass("justify-content-between");
             cardContent.addClass("align-items-center");
 
             var btnGroup = $("<div>");
-
             btnGroup.addClass("btn-group");
 
             var btn = $("<a>");
-
-            btn.addClass("btn");
-            btn.addClass("btn-sm");
-            btn.addClass("btn-outline-secondary");
-
+            btn.addClass("btn btn-sm btn-outline-secondary");
             btn.text("View");
-
             btn.attr({
                 href: item.url,
                 target: "_blank"
             });
 
             btnGroup.append(btn);
-
             cardContent.append(btnGroup);
-
             cardBody.append(cardContent);
-
             card.append(cardBody);
-
             page.append(card);
-
             pages.append(page);
         });
     });
