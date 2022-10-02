eabcf40a5ad4fe8a3c0b42adb6a8669aee986cbf
ATH <alex.t.hoffmann@icloud.com>
2022-10-02 22:22:43 +0200

    add NeoSolarized Theme inspired 404;

---
+++ traveller/js/404.js
@@ -0,0 +1,57 @@
+var particles = {
+  fpsLimit: 120,
+  particles: {
+    number: {
+      value: 3,
+      density: {
+        enable: true,
+        area: 100,
+      },
+    },
+    color: {
+      value: '#008781',
+    },
+    shape: {
+      type: 'circle',
+    },
+    opacity: {
+      value: 1,
+      random: {
+        enable: true,
+        minimumValue: 0.1,
+      },
+      animation: {
+        enable: true,
+        speed: 1,
+        minimumValue: 0,
+        sync: false,
+      },
+    },
+    size: {
+      value: 50,
+      random: {
+        enable: true,
+        minimumValue: 15,
+      },
+    },
+    move: {
+      enable: true,
+      speed: 17,
+      direction: 'down',
+      random: false,
+      straight: true,
+      outModes: {
+        default: 'out',
+      },
+    },
+  },
+  interactivity: {
+    detectsOn: 'canvas',
+    events: {
+      resize: false,
+    },
+  },
+  detectRetina: true,
+};
+
+tsParticles.load('tsparticles', particles);
