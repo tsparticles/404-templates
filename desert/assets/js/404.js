3999f67d4b7f351eebebb024a27421803ab1c0c5
Ahsan-Ehtesham <ahsanehtesham110@gmail.com>
2022-10-23 12:28:03 +0500

    Added desert 404 template

---
+++ desert/assets/js/404.js
@@ -0,0 +1,49 @@
+tsParticles.load("tsparticles", {
+  fpsLimit: 60,
+  particles: {
+    number: {
+      value: 800,
+      density: {
+        enable: true,
+        area: 1000,
+      },
+    },
+    color: {
+      value: "#ffffff",
+    },
+    shape: {
+      type: "circle",
+    },
+    opacity: {
+      value: 1,
+      random: {
+        enable: true,
+        minimumValue: 0.6,
+      },
+      animation: {
+        enable: true,
+        speed: 2,
+        minimumValue: 0,
+        sync: false,
+      },
+    },
+    size: {
+      value: 5,
+      random: {
+        enable: true,
+        minimumValue: 1,
+      },
+    },
+    move: {
+      enable: true,
+      speed: 0.9,
+      direction: "none",
+      random: true,
+      straight: false,
+      outModes: {
+        default: "out",
+      },
+    },
+  },
+  detectRetina: true,
+});
