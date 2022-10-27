96e4d1d5b8d19c58857d0c8678a75e0dbcc861a9
Ahsan-Ehtesham <ahsanehtesham110@gmail.com>
2022-10-27 14:11:01 +0500

    Updated 404.js config

--- desert/assets/js/404.js
+++ desert/assets/js/404.js
@@ -1,47 +1,54 @@
 tsParticles.load("tsparticles", {
   fpsLimit: 60,
   particles: {
-    number: {
-      value: 800,
-      density: {
-        enable: true,
-        area: 1000,
-      },
-    },
+    // The color of the particles/snowflakes
     color: {
-      value: "#ffffff",
-    },
-    shape: {
-      type: "circle",
-    },
-    opacity: {
-      value: 1,
-      random: {
-        enable: true,
-        minimumValue: 0.6,
-      },
+      value: "#fff",
       animation: {
         enable: true,
         speed: 2,
-        minimumValue: 0,
-        sync: false,
+        sync:true,
       },
     },
-    size: {
-      value: 5,
-      random: {
-        enable: true,
-        minimumValue: 1,
-      },
-    },
+    // Move the snow flakes to bottom for a realistic effect, "out" in outModes is for making them reappear on top once exited at the bottom of the page, the speed should be slow for a realistic effect
     move: {
+      direction: "bottom-right",
       enable: true,
-      speed: 0.9,
-      direction: "none",
-      random: true,
-      straight: false,
-      outModes: {
-        default: "out",
+      outModes: "out",
+      speed: 10,
+    },
+    // How many particles/snowflakes will be created when starting the effect, density is a good option to enable since it calculates the particles number based on the screen size (mobile users will be happy)
+    number: {
+      density: {
+        enable: true,
+        area: 800,
+      },
+      value: 500,
+    },
+    // The opacity of the particles/snowflakes
+    opacity: {
+      value: 1,
+    },
+    // The shape of the particles/snowflakes, also custom shapes can be used, this will be discussed at the end
+    shape: {
+      type: "circle",
+    },
+    // The size of the particles/snowflakes
+    size: {
+      value: 2,
+    },
+    // A nice wobble movement
+    wobble: {
+      enable: true,
+      distance: 10,
+      speed: 10,
+    },
+    // Some depth to the effect, (the layers count by default is 100, changing max here is not affecting that count)
+    // The zIndex will affect speed, size and opacity of the particles/snowflakes, the smaller the zIndex the smaller/more transparent/slower the particles/snowflakes will be
+    zIndex: {
+      value: {
+        min: 0,
+        max: 100,
       },
     },
   },
