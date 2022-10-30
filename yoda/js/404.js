c944a2940b28e0f1ebd994b07d3fe8d004224fe1
najwanan <najwanan@hotmail.com>
2022-10-30 00:21:16 -0700

    finalizing yoda 404

--- yoda/js/404.js
+++ yoda/js/404.js
@@ -11,16 +11,14 @@ var particles = {
             }
         },
         shape: {
-            type: "character",
-            character: {
-              value: ["circle"]
-            }
+            type: "circle",
+           
         },
         size: {
             value: 5,
             random: {
                 enable: true,
-                minimumValue: 10
+                minimumValue: 0.5
             }
         },
         move: {
@@ -35,5 +33,41 @@ var particles = {
     },
     detectRetina: true
 };
+
+/*
+shape: {
+            type: "circle"
+        },
+        opacity: {
+            value: 1,
+            random: {
+                enable: true,
+                minimumValue: 0.1
+            },
+            animation: {
+                enable: true,
+                speed: 1,
+                minimumValue: 0,
+                sync: false
+            }
+        },
+        size: {
+            value: 3,
+            random: {
+                enable: true,
+                minimumValue: 1
+            }
+        },
+        move: {
+            enable: true,
+            speed: 0.17,
+            direction: "none",
+            random: true,
+            straight: false,
+            outModes: {
+                default: "out"
+            },
+        }
+*/
 
 tsParticles.load("tsparticles", particles);
