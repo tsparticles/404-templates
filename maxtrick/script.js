64b918ec8a679b89ce3b0974e622deb6d171ef27
Khushal Sarode <105520366+Khushalsarode@users.noreply.github.com>
2023-10-26 21:37:29 +0530

    Update script.js

--- maxtrick/script.js
+++ maxtrick/script.js
@@ -6,47 +6,79 @@ toggleButton.addEventListener('click', () => {
 });
 
 
-    tsParticles.load("tsparticles", {
+tsParticles.load("tsparticles", {
+  fps_limit: 60,
   interactivity: {
+    detect_on: "canvas",
     events: {
-      onHover: {
+      onclick: { enable: true, mode: "push" },
+      onhover: {
         enable: true,
-        mode: "bubble"
+        mode: "attract",
+        parallax: { enable: false, force: 60, smooth: 10 }
       },
-      onClick: {
-        enable: true,
-        mode: "push"
-      }
+      resize: true
     },
     modes: {
-      bubble: {
-        opacity: 0.8,
-        size: 10,
-        color: {
-          value: "#ff0000"
-        }
-      }
+      push: { quantity: 4 },
+      attract: { distance: 200, duration: 0.4, factor: 5 }
     }
   },
   particles: {
-    color: {
-      value: "#000"
-    },
-    links: {
-      color: {
-        value: "#000"
-      },
+    color: { value: "#ffffff" },
+    line_linked: {
+      color: "#ffffff",
+      distance: 150,
       enable: true,
-      opacity: 0.5
+      opacity: 0.4,
+      width: 1
     },
     move: {
-      enable: true
+      attract: { enable: false, rotateX: 600, rotateY: 1200 },
+      bounce: false,
+      direction: "none",
+      enable: true,
+      out_mode: "out",
+      random: false,
+      speed: 2,
+      straight: false
     },
+    number: { density: { enable: true, value_area: 800 }, value: 80 },
     opacity: {
+      anim: { enable: false, opacity_min: 0.1, speed: 1, sync: false },
+      random: false,
       value: 0.5
     },
+    shape: {
+      character: {
+        fill: false,
+        font: "Verdana",
+        style: "",
+        value: "*",
+        weight: "400"
+      },
+      image: {
+        height: 100,
+        replace_color: true,
+        src: "images/github.svg",
+        width: 100
+      },
+      polygon: { nb_sides: 5 },
+      stroke: { color: "#000000", width: 0 },
+      type: "circle"
+    },
     size: {
-      value: 2
+      anim: { enable: false, size_min: 0.1, speed: 40, sync: false },
+      random: true,
+      value: 5
     }
-  }
+  },
+  polygon: {
+    draw: { enable: false, lineColor: "#ffffff", lineWidth: 0.5 },
+    move: { radius: 10 },
+    scale: 1,
+    type: "none",
+    url: ""
+  },
+  retina_detect: true
 });
